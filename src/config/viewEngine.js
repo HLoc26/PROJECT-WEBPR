e4861083b6eb856327039c76aff9b7a65482e8ab
HLoc26 <dhl26052004@gmail.com>
2024-10-31 11:43:04 +0700

    configuration for ejs

---
+++ src/config/viewEngine.js
@@ -0,0 +1,13 @@
+import { join } from "path";
+import express from "express";
+
+// Set view to render html from ./views/sample.ejs
+const configViewEngine = function (app) {
+	app.set("views", join("./src", "views"));
+	app.set("view engine", "ejs");
+
+	// Config static files
+	app.use(express.static(join("./src", "public")));
+};
+
+export default configViewEngine;
--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import "dotenv/config";
 import webRoutes from "./routes/web.js";
 import apiRoutes from "./routes/api.js";
+import configViewEngine from "./config/viewEngine.js";
 
 // Initialize express app
 const app = express();
 
+configViewEngine(app);
 // When route starts with "/", use webRoutes to handle
 app.use("/", webRoutes);
 // When route starts with "/api", use apiRoutes to handle
