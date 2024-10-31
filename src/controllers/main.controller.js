902f3dbac8ca3aeffdec1b11499383a125ce5acc
HLoc26 <dhl26052004@gmail.com>
2024-10-31 11:39:30 +0700

    handler for [GET] /news

--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -1,7 +1,9 @@
-const getHomepage = function (req, res) {
-	res.send("Hello from main.controller.js");
+import { getNews } from "./news.controller.js";
+export const getHomepage = function (req, res) {
+	res.send(`Hello from main.controller.js<br><a href="/news">Go to news</a>`);
 };
 
 export default {
 	getHomepage,
+	getNews,
 };
