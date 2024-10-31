7a271a17ed88ce97e0ce1f70f6a777b0e79c115b
HLoc26 <dhl26052004@gmail.com>
2024-10-31 11:22:57 +0700

    server configuration

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import "dotenv/config";
+import webRoutes from "./routes/web.js";
+import apiRoutes from "./routes/api.js";
+
+// Initialize express app
+const app = express();
+
+// When route starts with "/", use webRoutes to handle
+app.use("/", webRoutes);
+// When route starts with "/api", use apiRoutes to handle
+app.use("/api", apiRoutes);
+
+app.listen(PORT, function (req, res) {
+	console.log(`Listening on ${process.env.HOST_NAME}:${process.env.PORT}`);
+});
