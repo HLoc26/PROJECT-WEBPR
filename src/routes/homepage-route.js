f11775a20941fea70c4050b21e93664252630030
Sang-ute <nqs28012004@gmail.com>
2024-11-11 02:03:47 +0700

    Adding for list, will contact fe for more info

--- src/routes/homepage-route.js
+++ src/routes/homepage-route.js
@@ -1,8 +1,13 @@
 import express from "express";
 
 const router = express.Router();
+
 router.get("/Homepage", function (req, res) {
 	res.render("vwHomepage/Homepage");
 });
+
+router.get("/List", function(req,res){
+    res.render("vwHomepage/List");
+})
 
 export default Router;
