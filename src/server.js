511ac31fc0a2cf6a6642d21e3778292a0b123090
Sang-ute <nqs28012004@gmail.com>
2024-11-14 13:36:56 +0700

    Update route for editor

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 import homepageRoute from "./routes/homepage-route";
 app.use("/Homepage", homepageRoute);
 app.use("/List", homepageRoute);
+import editorRoute from "./routes/editor-route";
+app.use("/Editor", editorRoute);
