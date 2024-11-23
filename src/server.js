94f0d39a9afe93357572d3bc9a2cd02dce2b184b
Sang-ute <nqs28012004@gmail.com>
2024-11-23 23:39:32 +0700

    Update to import route from write-route

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
+import loginRoute from "./routes/login-route";
+import writerRoute from "./routes/writer-route";
 import homepageRoute from "./routes/homepage-route";
 app.use("/Homepage", homepageRoute);
 app.use("/List", homepageRoute);
+
 import editorRoute from "./routes/editor-route";
 app.use("/Editor", editorRoute);
