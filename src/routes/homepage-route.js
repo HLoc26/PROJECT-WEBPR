f883b308ee09cb9bdf08c8099458fffc4dcdcec9
Sang-ute <nqs28012004@gmail.com>
2024-11-23 22:34:53 +0700

    Adding homepage route, remove and add syntaxes

--- src/routes/homepage-route.js
+++ src/routes/homepage-route.js
@@ -2,12 +2,15 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/Homepage", function (req, res) {
+router.get("/", function (req, res) {
 	res.render("vwHomepage/Homepage");
 });
 
 router.get("/List", function(req,res){
     res.render("vwHomepage/List");
-})
+});
 
+router.get("/Category", function(res,req){
+    res.render("vwHomepage/Category");
+})
 export default Router;
