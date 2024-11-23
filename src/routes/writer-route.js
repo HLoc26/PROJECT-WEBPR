671ee6d540b20ffd1a059f03742de4a09cbe6ef2
Sang-ute <nqs28012004@gmail.com>
2024-11-23 23:38:17 +0700

    Adding function to render writer route

--- src/routes/writer-route.js
+++ src/routes/writer-route.js
@@ -2,8 +2,8 @@ import express from 'express';
 
 const router= express.Router();
 
-router.get('/', (req, res){
+router.get('/', function (req, res){
     res.render("vwWriter/Writer");
 });
 
-export default router;
+export default Router;
