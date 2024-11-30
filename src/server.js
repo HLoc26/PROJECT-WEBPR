249ecca381815cd9711795c1ea4c1cd4ded22394
Sang-ute <nqs28012004@gmail.com>
2024-11-30 23:22:33 +0700

    Replace order of routing in server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ import "dotenv/config";
 import webRoutes from "./routes/web.js";
 import apiRoutes from "./routes/api.js";
 import configViewEngine from "./config/viewEngine.js";
-
+import loginRoute from "./routes/login.routes.js";
+import writerRoute from "./routes/writer.routes.js";
+import homepageRoute from "./routes/homepage.routes.js";
+import editorRoute from "./routes/editor.routes.js";
 // Initialize express app
 const app = express();
 
@@ -13,17 +16,13 @@ app.use("/", webRoutes);
 // When route starts with "/api", use apiRoutes to handle
 app.use("/api", apiRoutes);
 
+app.use("/login", loginRoute);
+app.use("/writer", writerRoute);
+app.use("/homepage", homepageRoute);
+app.use("/list", homepageRoute);
+app.use("/Editor", editorRoute);
+
 app.listen(process.env.PORT, function (req, res) {
 	console.log(`Listening on ${process.env.HOST_NAME}:${process.env.PORT}`);
 });
 
-import loginRoute from "./routes/login-route.js";
-import writerRoute from "./routes/writer-route.js";
-import homepageRoute from "./routes/homepage-route.js";
-app.use("/Login", loginRoute);
-app.use("/Writer", writerRoute);
-app.use("/Homepage", homepageRoute);
-app.use("/List", homepageRoute);
-
-import editorRoute from "./routes/editor-route.js";
-app.use("/Editor", editorRoute);
