83f2a3586509efcc8c2699f150637ef84f4e6190
Nguyen Huy <huyngh05@gmail.com>
2024-12-06 16:27:36 +0700

    import path processing library + Add Middleware for static file + add 1 route --> server.js

--- src/server.js
+++ src/server.js
@@ -2,10 +2,22 @@ import express from "express";
 import "dotenv/config";
 import configViewEngine from "./config/viewEngine.js";
 
+import path from "path"; // Huy: add path processing library.
+
 // Initialize express app
 const app = express();
 
+// Huy: Middleware for static file (css, js, img,...)
+app.use(express.static(path.join(process.cwd(), "public")));
+
 configViewEngine(app);
+
+// Huy: add route
+app.get("/", function (req, res) {
+  res.render("layouts/reader.main", {
+    body: "Welcome to E-News!",
+  });
+});
 
 app.listen(process.env.PORT, function (req, res) {
 	console.log(`Listening on ${process.env.HOST_NAME}:${process.env.PORT}`);
