72bbbefa9842d9c94613cf005433302e60423fb1
Nguyen Huy <huyngh05@gmail.com>
2024-12-07 20:55:18 +0700

    Displays the basic layouts interface

--- src/server.js
+++ src/server.js
@@ -2,23 +2,22 @@ import express from "express";
 import "dotenv/config";
 import configViewEngine from "./config/viewEngine.js";
 
-import path from "path"; // Huy: add path processing library.
+import path from "path"; // Thư viện xử lý đường dẫn
 
 // Initialize express app
 const app = express();
 
-// Huy: Middleware for static file (css, js, img,...)
-app.use(express.static(path.join(process.cwd(), "public")));
-
 configViewEngine(app);
+
+app.use(express.static(path.join(process.cwd(), "public")));
 
 // Huy: add route
 app.get("/", function (req, res) {
-  res.render("layouts/reader.main", {
-    body: "Welcome to E-News!",
+  res.render("home", {
+    title: "Homepage",
   });
 });
 
 app.listen(process.env.PORT, function (req, res) {
-	console.log(`Listening on ${process.env.HOST_NAME}:${process.env.PORT}`);
+  console.log(`Listening on ${process.env.HOST_NAME}:${process.env.PORT}`);
 });
