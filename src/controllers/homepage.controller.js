45a97a24d81a9a8dc7e45d4bd3d72b30e1f2ded5
Sang-ute <nqs28012004@gmail.com>
2024-12-09 01:39:11 +0700

    Why côpilot not message, modify controller info

--- src/controllers/homepage.controller.js
+++ src/controllers/homepage.controller.js
@@ -6,6 +6,6 @@ export default {
     //logic lay cua service rap vao controller
     //du kien se co lay article, truoc mat chi render
     async GetHomepage (req, res) {
-        res.render("vwHomepage/Homepage",);//truyen user_id hay 
+        res.render("vwHomepage/Homepage",);//truyen top_view(service), newest_article hay gi tuy fe 
     }
 };
