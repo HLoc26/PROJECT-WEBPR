f402a528f34a0a6a106eba943443049d34c8c95a
HLoc26 <dhl26052004@gmail.com>
2024-12-09 17:19:36 +0700

    remove redundant files

--- src/controllers/main.controller.js
+++
@@ -1,9 +0,0 @@
-import { getNews } from "./news.controller.js";
-export const getHomepage = function (req, res) {
-	res.send(`Hello from main.controller.js<br><a href="/news">Go to news</a>`);
-};
-
-export default {
-	getHomepage,
-	getNews,
-};
