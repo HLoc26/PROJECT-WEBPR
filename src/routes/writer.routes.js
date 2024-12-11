1881b5454678767df146725ce264ffb72080fa46
Sang-ute <nqs28012004@gmail.com>
2024-12-11 20:21:29 +0700

    Adding edit route for the writer.routes.js, for edit option

--- src/routes/writer.routes.js
+++ src/routes/writer.routes.js
@@ -17,25 +17,24 @@ router.get("/new", function (req, res) {
 	});
 });
 router.get("/edit", async function (req, res) {
-    const id = req.query.id;
-    try {
+	const id = req.query.id;
+	try {
 		const article = await articleService.findArticleById(id);
 		if (!article) {
-            // Send a 404 response if the article is not found
-            return res.status(404).redirect("/error/500");
-        }
+			// Send a 404 response if the article is not found
+			return res.status(404).redirect("/error/500");
+		}
 
-        console.log(article); // Note: fixed the "edit" variable to "article"
-        res.render("vwWriter/edit", {
-            article: article,
-            layout: "layouts/admin.main.ejs",
-            api_key: process.env.TINY_API_KEY,
-        });
-    } catch (error) {
-        console.error("Error fetching article:", err);
-        res.status(500).redirect("/error/404")
-    }
+		console.log(article); // Note: fixed the "edit" variable to "article"
+		res.render("vwWriter/edit", {
+			article: article,
+			layout: "layouts/admin.main.ejs",
+			api_key: process.env.TINY_API_KEY,
+		});
+	} catch (error) {
+		console.error("Error fetching article:", err);
+		res.status(500).redirect("/error/404");
+	}
 });
-
 
 export default router;
