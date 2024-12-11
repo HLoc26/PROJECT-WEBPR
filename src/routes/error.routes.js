59a52bda29ec623261e88c900420f762086e6c3c
Sang-ute <nqs28012004@gmail.com>
2024-12-11 20:18:10 +0700

    Create error route for error message

---
+++ src/routes/error.routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+const router = express.Router();
+
+router.get("/404", function (req, res) {
+	res.render("vwError/404");
+});
+router.get("/500", function (req, res) {
+	res.render("vwError/500");
+});
+export default router;
