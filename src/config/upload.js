fd75d323f4ad4e8951eda8abe70cb9163e1cdbc8
HLoc26 <dhl26052004@gmail.com>
2024-12-12 18:52:20 +0700

    handle cover image upload

--- src/config/upload.js
+++ src/config/upload.js
@@ -1,6 +1,18 @@
 import express from "express";
 import multer from "multer";
+import path from "path";
 
-const upload = multer({ dest: "./src/public/img/" });
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "./src/public/img/");
+	},
+	filename: function (req, file, cb) {
+		const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+		const ext = path.extname(file.originalname); // Extract the original file extension
+		cb(null, file.fieldname + "-" + uniqueSuffix + ext); // Append the extension
+	},
+});
+
+const upload = multer({ storage: storage });
 
 export default upload;
