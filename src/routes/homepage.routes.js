1e52b5a7391134e2ccdfcff486013db0235f8f19
Sang-ute <nqs28012004@gmail.com>
2024-12-15 13:13:09 +0700

    Uncomment for the route to use controller

--- src/routes/homepage.routes.js
+++ src/routes/homepage.routes.js
@@ -1,14 +1,14 @@
 import express from "express";
-import homeController from '../controllers/homepage.controller.js';
+import homeController from "../controllers/homepage.controller.js";
 
 const router = express.Router();
 
-//router.get("/", homeController.GetHomepage);
-router.get("/", function (req, res) {
-	res.render("vwHomepage/Homepage");
-});
+router.get("/", homeController.GetHomepage);
+// router.get("/", function (req, res) {
+// 	res.render("vwHomepage/Homepage");
+// });
 
-router.get("/list", function(req,res){
+router.get("/list", function (req, res) {
 	res.render("vwHomepage/List");
 });
 export default router;
