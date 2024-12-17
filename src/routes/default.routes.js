f7d238dc78d537dacca727534a07c4a3e2485135
HLoc26 <dhl26052004@gmail.com>
2024-12-17 22:52:41 +0700

    using login layout for default routes

--- src/routes/default.routes.js
+++ src/routes/default.routes.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/register", function (req, res) {
-	res.render("vwLogin/Register", { layout: false });
+	res.render("vwLogin/Register", { layout: "layouts/login.main.ejs" });
 });
 
 router.post("/register", function (req, res) {
@@ -11,7 +11,7 @@ router.post("/register", function (req, res) {
 });
 
 router.get("/login", function (req, res) {
-	res.render("vwLogin/Login", { layout: false });
+	res.render("vwLogin/Login", { layout: "layouts/login.main.ejs" });
 });
 
 router.post("/login", function (req, res) {
@@ -19,18 +19,18 @@ router.post("/login", function (req, res) {
 });
 
 router.get("/forget", function (req, res) {
-	res.render("vwLogin/Forget", { layout: false });
+	res.render("vwLogin/Forget", { layout: "layouts/login.main.ejs" });
 });
 
 router.get("/OTP", function (req, res) {
-	res.render("vwLogin/OTP", { layout: false });
+	res.render("vwLogin/OTP", { layout: "layouts/login.main.ejs" });
 });
 
 router.get("/404", function (req, res) {
-	res.render("vwError/404", { layout: false });
+	res.render("vwError/404", { layout: "layouts/login.main.ejs" });
 });
 
 router.get("/500", function (req, res) {
-	res.render("vwError/500", { layout: false });
+	res.render("vwError/500", { layout: "layouts/login.main.ejs" });
 });
 export default router;
