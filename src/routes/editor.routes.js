1574d9fecf74caeb73e518c1a250bb7157bdb249
Sang-ute <nqs28012004@gmail.com>
2024-12-18 10:49:08 +0700

    Change route to render name correctly

--- src/routes/editor.routes.js
+++ src/routes/editor.routes.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", function (req, res) {
-	res.render("vwEditor/Editorpage");
+	res.render("vwEditor/editorhome");
 });
 
 export default router;
