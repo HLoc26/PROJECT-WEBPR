41e9ae5ffda80c9abfe8736fa341e960f4d414ab
Sang-ute <nqs28012004@gmail.com>
2024-12-18 14:58:54 +0700

    Fix syntax for article controller

--- src/controllers/article.controller.js
+++ src/controllers/article.controller.js
@@ -1,6 +1,3 @@
-import ArticleService from '../services/article.service.js'; 
-import CategoryService from '../services/category.service.js'; 
-
-export default{
-
-};
+import ArticleService from "../services/article.service.js";
+import CategoryService from "../services/category.service.js";
+export default {};
