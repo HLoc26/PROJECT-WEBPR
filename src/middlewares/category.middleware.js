980d6aeb07d2f9d93e22f8f75525869b0f285fdd
Nguyen Huy <huyngh05@gmail.com>
2024-12-18 09:27:07 +0700

    Initialize and configure middleware to set category variables --> middlewares/category.middleware.js

---
+++ src/middlewares/category.middleware.js
@@ -0,0 +1,28 @@
+import CategoryService from "../services/category.service.js";
+
+const setCategories = async (req, res, next) => {
+	try {
+		// Lấy danh sách tất cả các chuyên mục cấp 1 (parent)
+		const parentCategories = await CategoryService.findAllCategories().where("belong_to", null);
+
+		// Thêm các chuyên mục con (subcategories) vào từng chuyên mục cha
+		const categoriesWithSubcategories = await Promise.all(
+			parentCategories.map(async (category) => {
+				const subcategories = await CategoryService.findSubcategories(category.category_id);
+				return {
+					...category,
+					subcategories,
+				};
+			})
+		);
+
+		// Set biến categories để sử dụng trong các view
+		res.locals.categories = categoriesWithSubcategories;
+		next();
+	} catch (error) {
+		console.error(error);
+		next(error);
+	}
+};
+
+export default setCategories;
