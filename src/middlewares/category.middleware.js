574d86320d2bf23002a3e48d445f82218f5d8d0c
HLoc26 <dhl26052004@gmail.com>
2024-12-19 19:17:09 +0700

    update middlware to call the api instead of using services

--- src/middlewares/category.middleware.js
+++ src/middlewares/category.middleware.js
@@ -1,28 +1,14 @@
-import CategoryService from "../services/category.service.js";
+import apiClient from "../config/axios.js";
 
-const setCategories = async function (req, res, next) {
+export default async function setCategories(req, res, next) {
 	try {
-		// Lấy danh sách tất cả các chuyên mục cấp 1 (parent)
-		const parentCategories = await CategoryService.findAllCategories().where("belong_to", null);
-
-		// Thêm các chuyên mục con (subcategories) vào từng chuyên mục cha
-		const categoriesWithSubcategories = await Promise.all(
-			parentCategories.map(async function (category) {
-				const subcategories = await CategoryService.findSubcategories(category.category_id);
-				return {
-					...category,
-					subcategories,
-				};
-			})
-		);
-
+		const response = await apiClient.get("/api/categories");
 		// Set biến categories để sử dụng trong các view
-		res.locals.categories = categoriesWithSubcategories;
+		res.locals.categories = response.data;
+		console.log(response.data);
 		next();
 	} catch (error) {
 		console.error(error);
 		next(error);
 	}
-};
-
-export default setCategories;
+}
