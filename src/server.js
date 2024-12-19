45583dd9884906f9a3c1a93a5aa6ccf361a92713
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2024-12-19 12:28:36 +0700

    Add admin route to server

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import defaultRoute from "./routes/default.routes.js";
 import writerRoute from "./routes/writer.routes.js";
 import homepageRoute from "./routes/homepage.routes.js";
 import editorRoute from "./routes/editor.routes.js";
+import adminRoute from "./routes/admin.routes.js"
 
 import configViewEngine from "./config/viewEngine.js";
 
@@ -29,6 +30,7 @@ app.use("/writer", writerRoute);
 app.use("/homepage", homepageRoute);
 app.use("/list", homepageRoute);
 app.use("/editor", editorRoute);
+app.use("/admin", adminRoute)
 
 app.listen(process.env.PORT, function (req, res) {
 	console.log(`Listening on ${process.env.HOST_NAME}:${process.env.PORT}`);
