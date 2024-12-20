3cc80e190257f8cc20eda86b47fe05735f373059
Nguyen Huy <huyngh05@gmail.com>
2024-12-20 19:21:39 +0700

    Create a page to view a list of posts by tags

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,6 +1,7 @@
 // API routes here
 import { Router } from "express";
 import apiController from "../controllers/api.controller.js";
+import tagController from "../controllers/tag.controller.js";
 import upload from "../config/upload.js";
 
 const router = Router();
@@ -10,5 +11,7 @@ router.post("/images/upload", upload.single("image"), apiController.imgUpload);
 
 // API to get category list
 router.get("/categories", apiController.getCategories);
+
+router.get("/homepage/tag", tagController.getArticlesByTag);
 
 export default router;
