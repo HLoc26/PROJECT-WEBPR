da7021e583b61f88f3b414e7777067f7eec71b9f
HLoc26 <dhl26052004@gmail.com>
2024-12-21 21:48:56 +0700

    Add article creation with tag management in writer routes

--- src/routes/writer.routes.js
+++ src/routes/writer.routes.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import articleService from "../services/article.service.js";
 import express from "express";
 import upload from "../config/upload.js";
+import tagService from "../services/tag.service.js";
 const router = express.Router();
 
 router.get("/", async function (req, res) {
@@ -48,10 +49,64 @@ router.get("/edit", async function (req, res) {
 	}
 });
 
-router.post("/new", upload.single("thumbnail"), function (req, res) {
-	console.log(req.body);
-	console.log(req.file);
-	// const { status, title, summary, content, category } = req.body;
+router.post("/new", upload.single("thumbnail"), async function (req, res) {
+	try {
+		console.log(req.body);
+		console.log(req.file);
+		const { title, summary, content, category, premium, tags } = req.body;
+		const writer_id = req.session.user.user_id;
+		const is_premium = premium === "on" ? 1 : 0;
+		const tagsArr = tags
+			.split(",")
+			.map((tag) => tag.trim())
+			.filter((tag) => tag !== "");
+		const thumbnail = req.file ? req.file.filename : "default-thumbnail.jpg";
+
+		const entity = {
+			title: title,
+			content: content,
+			abstract: summary,
+			thumbnail: thumbnail,
+			views: 0,
+			status: "waiting",
+			published_date: null,
+			is_premium: is_premium,
+			writer_id: writer_id,
+			category_id: category,
+			editor_id: null,
+		};
+		console.log(entity);
+		const articleId = await articleService.addArticle(entity);
+
+		// Check if all the tags are already in the database if not, add them
+		// Get existing tags from DB
+		const tagsInDb = await tagService.findAllTags();
+		const existingTagNames = tagsInDb.map((tag) => tag.tag_name);
+
+		// console.log(existingTagNames);
+
+		// Process each tag
+		for (const tagName of tagsArr) {
+			let tagId;
+
+			// If tag doesn't exist in DB, add it
+			if (!existingTagNames.includes(tagName)) {
+				tagId = await tagService.addTag(tagName);
+			} else {
+				// Get the ID of existing tag
+				tagId = tagsInDb.find((tag) => tag.tag_name === tagName).tag_id;
+			}
+
+			// Add article-tag relationship
+			await tagService.addTagToArticle(articleId, tagId);
+		}
+
+		// Redirect to writer's dashboard or article list
+		res.redirect("/writer");
+	} catch (error) {
+		console.error("Error creating article:", error);
+		res.status(500).redirect("/500");
+	}
 });
 
 export default router;
