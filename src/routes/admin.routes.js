88104eb289c2484af7b5b4e91dd9afe6257c6d01
Sang-ute <nqs28012004@gmail.com>
2024-12-21 23:17:28 +0700

    Update route to use admin controller

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -2,6 +2,6 @@ import adminControllerSang from "../controllers/admin.controller.sang";
 import express from "express";
 const router = express.Router();
 
-router.get("/editor", adminControllerSang.fetchEditors);
+router.get("/editor", adminControllerSang.getEditors);
 
 export default router;
