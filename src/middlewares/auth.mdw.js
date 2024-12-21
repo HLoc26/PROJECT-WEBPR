0a9a9e8873624e9317f4b77cea6e7c8812551301
Bright <mastereziono1@gmail.com>
2024-12-21 12:40:43 +0700

    Change redirect status from /403 to /404 for editor and writer roles

--- src/middlewares/auth.mdw.js
+++ src/middlewares/auth.mdw.js
@@ -7,14 +7,14 @@ export function isAuth(req, res, next) {
 
 export function isEditor(req, res, next) {
   if (req.session.user?.user_role !== 'editor') {
-    return res.status(403).redirect('/403');
+    return res.status(403).redirect('/404');  // Changed from /403 to /404 (will be reconsidered)
   }
   next();
 }
 
 export function isWriter(req, res, next) {
   if (req.session.user?.user_role !== 'writer') {
-    return res.status(403).redirect('/403');
+    return res.status(403).redirect('/404');  // Changed from /403 to /404 (will be reconsidered)
   }
   next(); 
 }
