682eb3ad34add13ad257c65b67df02c994281ddb
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2024-12-21 11:31:14 +0700

    Update category articles route

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,7 +1,6 @@
 // API routes here
 import { Router } from "express";
 import apiController from "../controllers/api.controller.js";
-import {getCategoryArticles} from "../controllers/category.controller.js"
 import upload from "../config/upload.js";
 
 const router = Router();
@@ -12,6 +11,6 @@ router.post("/images/upload", upload.single("image"), apiController.imgUpload);
 // API to get category list
 router.get("/categories", apiController.getCategories);
 
-router.get('/homepage/cate', getCategoryArticles);
+router.get('/cate', apiController.getCategoryArticles);
 
 export default router;
