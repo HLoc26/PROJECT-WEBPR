73b4464f718bcc54f6fe9e12bc629c8b45d6b7bc
HLoc26 <dhl26052004@gmail.com>
2024-12-22 17:47:49 +0700

    prettier format

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -1,41 +1,34 @@
-import db from '../config/db.js'
-import knex from '../config/db.js';
+import db from "../config/db.js";
+import knex from "../config/db.js";
 
 const Article = db.Article;
 
-
 export default {
-    // Fetch all categories
-    findAllCategories() {
-      return db('categories');
-    },
-  
-    // Fetch a specific category by ID
-    findCategoryById(id) {
-      return db('categories').where('category_id', id).first();
-    },
+	// Fetch all categories
+	findAllCategories() {
+		return db("categories");
+	},
 
-    // Fetch all subcategories of a particular category
-    findSubcategories(parentCategoryId) {
-        return db('categories').where('belong_to', parentCategoryId);
-    },
+	// Fetch a specific category by ID
+	findCategoryById(id) {
+		return db("categories").where("category_id", id).first();
+	},
 
-    async findCategoryWithArticles(categoryId, page, limit) {
-    const offset = (page - 1) * limit;
+	// Fetch all subcategories of a particular category
+	findSubcategories(parentCategoryId) {
+		return db("categories").where("belong_to", parentCategoryId);
+	},
 
-    const articles = await knex('articles')
-        .where('category_id', categoryId)
-        .limit(limit)
-        .offset(offset);
+	async findCategoryWithArticles(categoryId, page, limit) {
+		const offset = (page - 1) * limit;
 
-    const totalArticles = await knex('articles')
-        .where('category_id', categoryId)
-        .count('article_id as count');
-    
+		const articles = await knex("articles").where("category_id", categoryId).limit(limit).offset(offset);
 
-    const totalPages = Math.ceil(totalArticles[0].count / limit);
+		const totalArticles = await knex("articles").where("category_id", categoryId).count("article_id as count");
 
-    return { articles, totalPages };
-}
+		const totalPages = Math.ceil(totalArticles[0].count / limit);
+
+		return { articles, totalPages };
+	},
+	findEditorCategories(editor_id) {},
 };
-  
