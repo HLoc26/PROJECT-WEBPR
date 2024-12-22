03d04b4670fdda37481f800e19d3e847027d133f
Sang-ute <nqs28012004@gmail.com>
2024-12-22 18:45:18 +0700

    Update POST route to update and delete editor

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 router.get("/editor", adminControllerSang.getEditors);
 router.get("/editor/detail", adminControllerSang.getEditorDetails);
+router.post("/editor/:user_id/delete", adminControllerSang.deleteUser);
+router.post("/editor/:user_id/submit", adminControllerSang.updateProfile);
 
 export default router;
