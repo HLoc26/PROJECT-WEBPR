193a0aaca54076b2cecb6459dd4d8816fa043220
Nguyen Huy <huyngh05@gmail.com>
2024-12-23 15:38:28 +0700

    Create router and controller to handle search

--- src/routes/article.routes.js
+++ src/routes/article.routes.js
@@ -1,9 +1,11 @@
-import express from 'express';
-import articleController from '../controllers/article.controller.js';
+import express from "express";
+import articleController from "../controllers/article.controller.js";
 
 const router = express.Router();
 
 router.get("/:id", articleController.getArticleDetail);
 router.post("/:id/comment", articleController.postComment);
+
+router.get("/search", articleController.search);
 
 export default router;
