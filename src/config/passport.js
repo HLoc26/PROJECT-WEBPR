f767ae447ed2fdadbd920a0839c574d0e2063b54
Bright <mastereziono1@gmail.com>
2024-12-23 20:59:27 +0700

    update passport.js to handle new OAuth user

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,7 +1,6 @@
-import passport from "passport";
-import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import { Strategy as FacebookStrategy } from "passport-facebook";
-import userService from "../services/user.service.js";
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import userService from '../services/user.service.js';
 
 export default function configurePassport(app) {
   app.use(passport.initialize());
@@ -20,59 +19,28 @@ export default function configurePassport(app) {
     }
   });
 
-  // Google Strategy
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "/auth/google/callback"
   }, async (accessToken, refreshToken, profile, done) => {
     try {
       let user = await userService.findByEmail(profile.emails[0].value);
       
       if (!user) {
         const newUser = {
-          username: profile.displayName.replace(/\s/g, '').toLowerCase(),
+          username: profile.displayName.toLowerCase().replace(/\s/g, ''),
           email: profile.emails[0].value,
           full_name: profile.displayName,
           user_role: 'reader',
-          is_active: 0, // Set to inactive until registration is complete
-          password: null // OAuth users don't need password
+          is_active: 0,
+          oauth_provider: 'google',
+          oauth_id: profile.id
         };
         
-        await userService.addReader(newUser);
-        user = await userService.findByEmail(profile.emails[0].value);
-        user.isNewUser = true; // Mark as new user
-      }
-      
-      return done(null, user);
-    } catch (err) {
-      return done(err, null);
-    }
-  }));
-
-  // Facebook Strategy
-  passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_APP_ID,
-    clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/callback",
-    profileFields: ['id', 'emails', 'name']
-  }, async (accessToken, refreshToken, profile, done) => {
-    try {
-      let user = await userService.findByEmail(profile.emails[0].value);
-      
-      if (!user) {
-        const newUser = {
-          username: `fb_${profile.id}`,
-          email: profile.emails[0].value,
-          full_name: `${profile.name.givenName} ${profile.name.familyName}`,
-          user_role: 'reader',
-          is_active: 0, // Set to inactive until registration is complete
-          password: null
-        };
-        
-        await userService.addReader(newUser);
-        user = await userService.findByEmail(profile.emails[0].value);
-        user.isNewUser = true; // Mark as new user
+        const userId = await userService.addOAuthUser(newUser);
+        user = await userService.findUserById(userId);
+        user.isNewUser = true;
       }
       
       return done(null, user);
