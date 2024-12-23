4f353e26f51de0dc98c7245141613209daaf871c
Nguyen Huy <huyngh05@gmail.com>
2024-12-24 05:01:59 +0700

    Fix the path in the route

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,15 +1,15 @@
 import adminControllerSang from "../controllers/admin.controller.sang.js";
-import adminController from '../controllers/admin.controller.js'; 
+import adminController from "../controllers/admin.controller.js";
 import express from "express";
 import adminControllerHuy from "../controllers/admin.controller.huy.js";
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    adminController.showArticlesPage(req, res);
+router.get("/", (req, res) => {
+	adminController.showArticlesPage(req, res);
 });
 
-router.get("/writers", adminController.getWriters)
-router.get('/writers/detail', adminController.getWriterDetails);
+router.get("/writers", adminController.getWriters);
+router.get("/writers/detail", adminController.getWriterDetails);
 
 router.get("/editor", adminControllerSang.getEditors);
 router.get("/editor/detail", adminControllerSang.getEditorDetails);
@@ -19,7 +19,7 @@ router.post("/editor/:user_id/submit", adminControllerSang.updateProfile);
 // Category managementAdd routers to manage categories
 router.get("/categories", adminControllerHuy.getCategories);
 router.get("/categories/add", adminControllerHuy.getAddCategories);
-router.post("categories/add", adminControllerHuy.addCategories);
+router.post("/categories/add", adminControllerHuy.addCategories);
 router.get("/categories/edit", adminControllerHuy.getEditCategories);
 router.post("/categories/patch", adminControllerHuy.editCategories);
 
