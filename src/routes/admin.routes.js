add65e66bec88187065980fc7becb8d81034c12a
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2024-12-23 23:49:20 +0700

    Add tag management routes to admin routes

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,6 +1,7 @@
 
 import adminControllerSang from "../controllers/admin.controller.sang.js";
 import adminController from '../controllers/admin.controller.js'; 
+import adminTagsController from "../controllers/tag.controller.js";
 import express from "express";
 const router = express.Router();
 
@@ -9,7 +10,13 @@ router.get('/', (req, res) => {
 });
 
 router.get("/writers", adminController.getWriters)
-router.get('/writers/detail', adminController.getWriterDetails);
+router.get("/writers/detail", adminController.getWriterDetails);
+
+router.get("/tags", adminTagsController.getAllTags);
+router.get("/tags/:id", adminTagsController.getTagById);
+router.post("/tags/add", adminTagsController.addTag);
+router.post("/tags/:id/edit", adminTagsController.updateTag);
+router.post("/tags/:id/delete", adminTagsController.deleteTag);
 
 router.get("/editor", adminControllerSang.getEditors);
 router.get("/editor/detail", adminControllerSang.getEditorDetails);
