1c6fab4dae9698402584191ec893e8c44fe3bef7
Bright <mastereziono1@gmail.com>
2024-12-23 16:58:28 +0700

    Add method to retrieve top 10 categories by article views

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -34,4 +34,14 @@ export default {
 	findEditorCategory(editor_id) {
 		return db("categories").join("users", "users.managed_category_id", "categories.category_id").where("users.user_id", editor_id).select("categories.*");
 	},
+
+	getTop10Views() {
+		return db("categories as c")
+			.leftJoin("articles as a", "c.category_id", "a.category_id")
+			.select("c.category_id")
+			.sum("a.views as total_views")
+			.groupBy("c.category_id")
+			.orderBy("total_views", "desc")
+			.limit(10);
+	},
 };
