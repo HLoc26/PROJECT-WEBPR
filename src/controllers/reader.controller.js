e6ab7fb705c2afbd135cb5c271a069fb46be2df3
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2024-12-24 02:38:43 +0700

    Add getUserDetails method to reader.controller.js

--- src/controllers/reader.controller.js
+++ src/controllers/reader.controller.js
@@ -12,4 +12,19 @@ export default {
         }
     },
 
+    // Get information of a specific user
+    async getUserDetails(req, res) {
+        try {
+            const userId = req.params.id;
+            const user = await UserService.findUserById(userId);
+            if (!user) {
+                return res.status(404).render("vwError/404", { message: "User not found" });
+            }
+            res.render("../views/vwAdmin/UserDetails.ejs", { user });
+        } catch (error) {
+            console.error("Error in getUserDetails:", error);
+            res.status(500).render("vwError/500", { message: "Internal Server Error" });
+        }
+    },
+
 };
