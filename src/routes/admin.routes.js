16ebd5822bde082f33e8c47b1f45f8c03a3ffbda
HLoc26 <dhl26052004@gmail.com>
2024-12-24 08:42:05 +0700

    Refactor admin routes to consolidate tag management under adminController

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,22 +1,20 @@
-
 import adminControllerSang from "../controllers/admin.controller.sang.js";
-import adminController from '../controllers/admin.controller.js'; 
-import adminTagsController from "../controllers/tag.controller.js";
+import adminController from "../controllers/admin.controller.js";
 import express from "express";
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    adminController.showArticlesPage(req, res);
+router.get("/", (req, res) => {
+	adminController.showArticlesPage(req, res);
 });
 
-router.get("/writers", adminController.getWriters)
+router.get("/writers", adminController.getWriters);
 router.get("/writers/detail", adminController.getWriterDetails);
 
-router.get("/tags", adminTagsController.getAllTags);
-router.get("/tags/:id", adminTagsController.getTagById);
-router.post("/tags/add", adminTagsController.addTag);
-router.post("/tags/:id/edit", adminTagsController.updateTag);
-router.post("/tags/:id/delete", adminTagsController.deleteTag);
+router.get("/tags", adminController.getAllTags);
+router.get("/tags/:id", adminController.getTagById);
+router.post("/tags/add", adminController.addTag);
+router.post("/tags/:id/edit", adminController.updateTag);
+router.post("/tags/:id/delete", adminController.deleteTag);
 
 router.get("/editor", adminControllerSang.getEditors);
 router.get("/editor/detail", adminControllerSang.getEditorDetails);
