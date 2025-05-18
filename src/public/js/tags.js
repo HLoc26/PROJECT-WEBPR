e6f3cf12e8b94cd26d4c37d3100b1eddeebde3ba
HLoc26 <dhl26052004@gmail.com>
2025-05-19 01:05:26 +0700

    chore: move inline style and script to seperated files

---
+++ src/public/js/tags.js
@@ -0,0 +1,9 @@
+function showAddTagForm() {
+	document.getElementById("overlay").style.display = "block";
+	document.getElementById("addTagForm").style.display = "block";
+}
+
+function hideAddTagForm() {
+	document.getElementById("overlay").style.display = "none";
+	document.getElementById("addTagForm").style.display = "none";
+}
